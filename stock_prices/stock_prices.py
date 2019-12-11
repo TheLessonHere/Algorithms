e416edf84cce6ec3a36beea5ffc82763e4d9a517
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-12-11 16:47:39 -0700

    Initial commit, Stock Prices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,32 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # Set current minimum price to the first item in the list to start
+    cur_min_price = prices[0]
+  # Set maximum profit so far to a ridiculously low number to start
+    max_profit = -10**10001
+    # Loop over the array checking each price against the current minimum price
+    for i in range(len(prices)):
+      # If the current indexed price is higher than the minimum, check its profit margin against the current minimum price
+      if prices[i] > cur_min_price:
+        profit = prices[i] - cur_min_price
 
+        # Then if the profit margin returned is higher than the current max profit, it becomes the new max profit
+        if profit > max_profit:
+          max_profit = profit
+
+      # In the case that the prices on the list grow smaller and smaller and there is never a positive max profit
+      elif i == len(prices) - 1 and prices[i] < cur_min_price:
+        # Sets the max profit to the least amount of money we could have possible lost,
+        # which is the min price - the previous min price
+        if prices[i] - cur_min_price > max_profit:
+          max_profit = prices[i] - cur_min_price
+
+      # If the current indexed price is lower than the current minimum it becomes the new minimum
+      elif prices[i] < cur_min_price:
+        cur_min_price = prices[i]
+
+    return max_profit
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
