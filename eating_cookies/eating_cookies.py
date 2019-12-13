66a736f35d0fda3d9dbf14c0950df745f5f5ef98
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-12-12 20:50:44 -0700

    MVP met

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,10 +3,28 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+
+def eating_cookies(n, cache = None):
+  # Initialize cache
+  if cache is None:
+    cache = [0 for i in range(n + 1)]
+  # handle case for n = 0
+  cache[0] = 1
+  # handle case for n < 0
+  if n < 0:
+    return 0
+  # If there is no value in the cache, set a permutation counter
+  # for the range of combinable numbers (0, 1, 2, 3), find the sum
+  # of the permutations by recursively adding them together
+  elif cache[n] == 0:
+    permutations = 0
+    for amount in range(1, 4):
+      permutations += eating_cookies(n - amount, cache)
+    cache[n] = permutations
+  # If n is in the cache, return that value to be added to the permutation total
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,28 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  # Initialize cache
+  cache = [0 for i in range(amount + 1)]
+  # Handle case for amount = 0 cents
+  cache[0] = 1
+  # Define nested recursive function to iterate over each amount and denomination
+  def recur_change(amount, denominations, cache):
+    # If we hit negatives, return 0 (i.e. if an amount is less than the coin we are checking)
+    if amount < 0:
+      return 0
+    # Otherwise if the amount has not already been checked and cached...
+    elif cache[amount] == 0:
+      # Iterate over each possible coin amount in denominations
+      for coin in denominations:
+        # For each coin, iterate over the higher amounts between the coin value and the starting amount
+        for higher_amount in range(coin, amount + 1):
+          # For each amount (higher_amount), set the value of the cache for the higher amount equal to the recursive sum
+          # decrementing the amount by the coin each time until it hits our base case
+          cache[higher_amount] += recur_change(higher_amount - coin, denominations, cache)
+    # If the amount HAS already been checked and cached, return the number for the given amount to end the recursion
+    return cache[amount]
+  # Call our recursive function to begin the loop
+  return recur_change(amount, denominations, cache)
 
 
 if __name__ == "__main__":
